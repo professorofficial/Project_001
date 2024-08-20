943ad714606a672c3ffbf6b1c291673aea2aa67b
Surya Muthumanickam <suryabangbang07@gmail.com>
2024-08-20 18:51:55 +0530

    sustainabyte_ai

---
+++ script.js
@@ -0,0 +1,103 @@
+function isEven(num) {
+ return num % 2 === 0;
+ }
+ console.log(isEven(4)); // true
+ console.log(isEven(5)); // false
+
+ /**
+  * Check if the given number is even.
+  *
+  * @param {number} num - The number to be checked.
+  * @return {boolean} true if the number is even, false otherwise.
+  */
+ function isEven(num) {
+    reuturn num % 2 === 0;
+ }
+ console.log(isEven(4)); // true
+ console.log(isEven(5)); // false
+
+ function isEven(num) {
+    return num % 2 === 0;
+ }
+ console.log(isEven(4)); // true
+ console.log(isEven(5)); // false
+
+ function isEven(num) {
+    return num % 2 === 0;
+ }
+ console.log(isEven(4)); //true
+ console.log(isEven(5)); //false
+
+ function isEven(num) {
+    return num % 2 === 0;
+ }
+ console.log(isEven(4)); //true
+ console.log(isEven(5)); //false
+
+ function isEven(num) {
+    return num % 2 === 0;
+ }
+ console.log(isEven(4)); //true
+ console.log(isEven(5)); //false
+
+ function isEven(num) {
+    return num % 2 === 0;
+ }
+console.log(isEven(4)); //true
+console.log(isEven(5)); //false
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(isEven(4)); //true
+console.log(isEven(5)); //false
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(isEven(4)); //true
+console.log(isEven(5)); //false
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(isEven(4)); //true
+console.log(isEven(5)); //false
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(isEven(4)); //true
+console.log(isEven(5)); //false
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(isEven(4)); //true
+console.log(isEven(5)); //false
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+console.log(isEven(4)); //true
+console.log(isEven(5)); //false
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(isEven(4)); //true
+console.log(isEven(5)); //false
+
+function isEven(num) {
+    return num % 2 ===0;
+}
+console.log(isEven(4)): //true
+console.log(isEven(5)); //false
+
+funtion isEven(num) {
+    return num % 2 === 0;
+}
+
+console.log(isEven(4)); //true
+console.log(isEven(5)); //false
